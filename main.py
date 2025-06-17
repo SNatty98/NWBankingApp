67f160a6e5740c1d5dea281e26fca26b8d717809
SNatty98 <mrnatt98@gmail.com>
2025-06-17 20:41:37 +0100

    Separated tables into Money in/out and added Category

--- main.py
+++ main.py
@@ -13,12 +13,11 @@ def load_transactions(file):
     try:
         df = pd.read_csv(file, encoding='latin-1', skiprows=3)
         df.columns = [col.strip() for col in df.columns]
+        df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
         df["Paid out"] = df["Paid out"].str.replace(
             ",", "").str.replace("£", "").astype(float)
         df["Paid in"] = df["Paid in"].str.replace(
             ",", "").str.replace("£", "").astype(float)
-        df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
-        st.write(df)
         return df
     except Exception as e:
         st.error(f"Error processing file:  {str(e)}")
@@ -33,6 +32,20 @@ def main():
 
     if uploaded_file is not None:
         df = load_transactions(uploaded_file)
+        df["Category"] = "No Category"
+
+        if df is not None:
+            out_df = df[df["Paid out"].notnull()].copy()
+            in_df = df[df["Paid in"].notnull()].copy()
+
+            tab1, tab2 = st.tabs(["Money out", "Money In"])
+
+            with tab1:
+                filter_out_df = out_df.drop(columns=["Paid in", "Balance"])
+                st.write(filter_out_df)
+            with tab2:
+                filter_in_df = in_df.drop(columns=["Paid out", "Balance"])
+                st.write(filter_in_df)
 
 
 main()
